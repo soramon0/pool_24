087bba5b53811a9cdc10833c14db6168037ee1ca
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-07-25 10:47:26 +0100

    ex07: iterative apporach

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -19,24 +19,26 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	long num;
+	char	c[10];
+	long	num;
+	int		i;
 
-	num = (long)nb;
+	num = nb;
+	i = 0;
 	if (num < 0) {
 		num *= -1;
 		ft_putchar('-');
 	}
-	if (num <= 9)
+	if (num >= 0 && num <= 9)
 	{
 		ft_putchar(num + '0');
-		return;
+		return ;
 	}
-	ft_putnbr(num / 10);
-	ft_putnbr(num % 10);
-}
-
-int	main(void)
-{
-	ft_putnbr(-2147483648);
-	return (0);
+	while (num % 10)
+	{
+		c[i++] = num % 10 + '0';
+		num /= 10;
+	}
+	while(i >= 0)
+		ft_putchar(c[--i]);
 }
