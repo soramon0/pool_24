09496d861021a0ba2976e4da8a488a59da10dfd8
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-07-25 23:49:38 +0100

    fix: ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -12,20 +12,21 @@ void ft_swap(int *a, int *b)
 void  ft_sort_int_tab(int *tab, int size)
 {
   int i;
-  int j;
 
   if (size <= 1)
     return ;
   i = 0;
-  while(i < size)
+  while(size >= 0)
   {
-    j = i;
-    while(tab[j] > tab[j + 1])
+    while (i < size - 1)
     {
-      ft_swap(&tab[j], &tab[j + 1]);
-      j++;
+      if (tab[i] > tab[i + 1])
+      {
+        ft_swap(&tab[i], &tab[i + 1]);
+      }
+      i++;
     }
-    i++;
+    size--;
   }
 }
 
