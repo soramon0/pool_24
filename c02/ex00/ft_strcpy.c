20291d26b2c96243f862ba75bcdbe66c516fc6d7
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-07-28 15:44:32 +0100

    c02: ex0{1..2}

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,24 @@
+char  *ft_strcpy(char *dest, const char *src)
+{
+  int i;
+
+  i = 0;
+  while(src[i] != '\0')
+  {
+    dest[i] = src[i];
+    i++;
+  }
+  dest[i] = '\0';
+  return dest;
+}
+
+/*
+int main()
+{
+  char src[] = "Hello World";
+  char dest[12];
+  ft_strcpy(dest, src);
+  printf("src = %s - dest = %s\n", src, dest);
+  return 0;
+}
+*/
---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+char  *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+  unsigned int i;
+
+  i = 0;
+  while (i < n && src[i] != '\0')
+  {
+    dest[i] = src[i];
+    i++;
+  }
+  while(i < n)
+  {
+    dest[i] = '\0';
+    i++;
+  }
+
+  return dest;
+}
+
+int main()
+{
+  char src[] = "karim is here yo";
+  char dest[6];
+  ft_strncpy(dest, src, 6);
+  printf("src = %s - dest = %s\n", src, dest);
+  return 0;
+}
