188da382bb41d1560bfa1f16c436ca525fa18d46
Karim Laayoun <klaayoun@e1r3p7.1337.ma>
2024-08-01 12:30:06 +0100

    c03: e05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -28,35 +28,31 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 
 	s1_len = ft_strlen(dest);
 	s2_len = ft_strlen(src);
-	i = 0;
 	if (size == 0)
 		return (s2_len);
-	while (src[i])
+	i = s1_len;
+	while (src[i - s1_len] && i < size - 1)
 	{
-		if ((s1_len + i) < size - 1)
-			dest[s1_len + i] = src[i];
+		dest[i] = src[i - s1_len];
 		i++;
 	}
+	dest[i] = '\0';
 	if (size > i)
-	{
-		dest[s1_len + i] = '\0';
-		return (s1_len + i);
-	}
-	dest[s1_len + size] = '\0';
-	return (s1_len + size);
+		return (s1_len + s2_len);
+	return (s1_len + i);
 }
 // #include <string.h>
 // #include <stdio.h>
 // int main()
 // {
-// 	char a1[10] = "aa";
-// 	char a2[] = "bb";
-// 	unsigned int nb = 6;
+// 	char a1[10] = "aaaa";
+// 	char a2[] = "bbbbbbbbbbbbbbbbbbbb";
+// 	unsigned int nb = 10;
 // 	int r1 = strlcat(a1, a2, nb);
 // 	printf("strlcat(\"%s\", \"%s\", %d) = %d\n", a1, a2, nb, r1);
 //
-// 	char b1[10] = "aa";
-// 	char b2[] = "bb";
+// 	char b1[10] = "aaaa";
+// 	char b2[] = "bbbbbbbbbbbbbbbbbbbb";
 // 	int r2 = ft_strlcat(b1, b2, nb);
 // 	printf("ft_strcat(\"%s\", \"%s\", %d) = %d\n", b1, b2, nb, r2);
 // 	return (0);
