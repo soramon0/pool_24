bbe494db1776685d19ccd65d377d34451df477f5
Karim Laayoun <klaayoun@e1r3p7.1337.ma>
2024-08-05 17:57:44 +0100

    c06

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klaayoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 14:38:37 by klaayoun          #+#    #+#             */
+/*   Updated: 2024/08/05 17:36:24 by klaayoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_sort_strings(char **tab, int size)
+{
+	int	i;
+	int	j;
+	int	swapped;
+
+	if (size <= 1)
+		return ;
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		swapped = 0;
+		while (j < size - i - 1)
+		{
+			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			{
+				ft_swap(&tab[j], &tab[j + 1]);
+				swapped = 1;
+			}
+			j++;
+		}
+		if (swapped == 0)
+			break ;
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	argv++;
+	ft_sort_strings(argv, argc - 1);
+	while (*argv)
+	{
+		ft_putstr(*argv);
+		write(1, "\n", 1);
+		argv++;
+	}
+	return (0);
+}
