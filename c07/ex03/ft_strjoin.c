33d150592ff5536ea42716585f0b340d5ebf823a
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-08-07 23:52:27 +0100

    c07: ex03 - str_join

---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klaayoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 18:51:35 by klaayoun          #+#    #+#             */
+/*   Updated: 2024/08/07 18:51:36 by klaayoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int ft_count_strs(int size, char **strs)
+{
+  int count;
+  int i;
+  int j;
+
+  i = 0;
+  while (i < size && strs[i])
+  {
+    j = 0;
+    while (strs[i][j++])
+      count++;
+    i++;
+  }
+  return (count);
+}
+
+char *alloc_str(int size, char **strs, char *sep)
+{
+  int count;
+  int i;
+
+  count = ft_count_strs(size, strs);
+  i = 0;
+  while(sep[i] != '\0')
+    i++;
+  count = (count + (i * size)) - i;
+  return ((char *)malloc(sizeof(char) * (count + 1)));
+}
+
+char  *ft_strjoin(int size, char **strs, char *sep)
+{
+  int   i;
+  int   j;
+  int   offset;
+  char  *join;
+
+  if (size == 0)
+    return ((char *)(malloc(0)));
+  join = alloc_str(size, strs, sep);
+  if (!join)
+    return (NULL);
+  offset = 0;
+  i = 0;
+  while (i < size && strs[i])
+  {
+    j = 0;
+    while (strs[i][j])
+     join[offset++] = strs[i][j++];
+    j = 0;
+    while (sep[j] && i != size - 1)
+      join[offset++] = sep[j++];
+    i++; 
+  }
+  join[offset] = '\0';
+  return (join);
+}
+// #include <stdio.h>
+// int main(void)
+// {
+//   char *strs[] = {"Hello", "World", "69"};
+//   char *result = ft_strjoin(3, strs, ", ");
+//   printf("%s\n", result);
+//   free(result);
+//   return (0);
+// }
