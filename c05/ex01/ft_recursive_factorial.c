2775e98dae55d30ea5d6268b8b9e7e67a0dc97ec
Karim Laayoun <klaayoun@e1r3p7.1337.ma>
2024-08-12 15:25:24 +0100

    c05-ex01: wrong fn name

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -10,13 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
 	if (nb == 0)
 		return (1);
-	return (nb * ft_iterative_factorial(nb - 1));
+	return (nb * ft_recursive_factorial(nb - 1));
 }
 // #include <stdio.h>
 // int	main(void)
