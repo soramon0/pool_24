5d8e30ed534dfb9677cf1a22dbff245e43d6e0ad
Karim Laayoun <klaayoun@e1r3p7.1337.ma>
2024-08-12 14:09:22 +0100

    c07: fix negative range

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,38 +6,36 @@
 /*   By: klaayoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 21:26:42 by klaayoun          #+#    #+#             */
-/*   Updated: 2024/08/06 21:26:44 by klaayoun         ###   ########.fr       */
+/*   Updated: 2024/08/08 13:31:53 by klaayoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int *ft_range(int min, int max)
+int	*ft_range(int min, int max)
 {
-  int *r;
-  int i;
-  int size;
+	int	*r;
+	int	i;
+	int	size;
 
-  if (min < 0)
-    return (0);
-  if (min >= max)
-    return (0);
-  size = max - min;
-  r = malloc(sizeof(int) * size);
-  if (!r)
-    return (0);
-  i = 0;
-  while (i < size)
-  {
-    r[i] = min + i;
-    i++;
-  }
-  return (r);
+	if (min >= max)
+		return (NULL);
+	size = max - min;
+	r = malloc(sizeof(int) * size);
+	if (!r)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		r[i] = min + i;
+		i++;
+	}
+	return (r);
 }
 // #include <stdio.h>
 // int main(void)
 // {
-//   int min = 1;
+//   int min = -2;
 //   int max = 3;
 //   int *data = ft_range(min, max);
 //   int i = 0;
--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,38 +6,36 @@
 /*   By: klaayoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:11:55 by klaayoun          #+#    #+#             */
-/*   Updated: 2024/08/07 17:11:58 by klaayoun         ###   ########.fr       */
+/*   Updated: 2024/08/08 13:32:54 by klaayoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int ft_utlimate_range(int **range, int min, int max)
+int	ft_utlimate_range(int **range, int min, int max)
 {
-  int i = 0;
-  int size;
+	int	i;
+	int	size;
 
-  *range = NULL;
-  if (min < 0)
-    return (0);
-  if (min >= max)
-    return (0);
-  size = max - min;
-  *range = (int *)malloc(sizeof(int) * size);
-  if (!range)
-    return (-1);
-  i = 0;
-  while (i < size)
-  {
-    (*range)[i] = min + i;
-    i++;
-  }
-  return (i);
+	*range = NULL;
+	if (min >= max)
+		return (0);
+	size = max - min;
+	*range = (int *)malloc(sizeof(int) * size);
+	if (!range)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (i);
 }
 // #include <stdio.h>
 // int main(void)
 // {
-//   int min = 0;
+//   int min = -5;
 //   int max = 5;
 //   int *range;
 //   int size = ft_utlimate_range(&range, min, max);
