ae8c0a2b3665c178485f678410954124a7a87037
Karim Laayoun <klaayoun@e1r3p7.1337.ma>
2024-08-14 22:41:09 +0100

    c09: ex00

---
+++ c09/ex00/libft_creator.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TARGET_LIB_FILES="ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c"
+OBJECT_LIB_FILES="ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o"
+LIB_NAME="libft.a"
+
+cc -Wall -Wextra -Werror -c $TARGET_LIB_FILES
+ar rc $LIB_NAME $OBJECT_LIB_FILES
+ranlib $LIB_NAME
+rm -f $OBJECT_LIB_FILES
